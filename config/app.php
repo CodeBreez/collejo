6ffe9f5fee6a05634a02b2833fd090922ce01699
Anuradha Jayathilaka <astroanu2004@gmail.com>
2016-06-02 15:09:00 +0000

    removed migrations

--- config/app.php
+++ config/app.php
@@ -203,6 +203,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Uuid' => Webpatser\Uuid\Uuid::class
     ],
 
 ];
