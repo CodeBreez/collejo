161e9c5f74726f35dfd9457646e34b0ab57b525f
Anuradha <astroanu2004@gmail.com>
2016-06-22 23:41:46 +0530

    moved students to app repo

--- modules/Students/Http/menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Menu::group('Students', 'fa-users', function($parent){
-
-	Menu::create('students.list', 'List')->setParent($parent);
-	Menu::create('students.new', 'Create New')->setParent($parent);
-	
-});
--- modules/Students/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'dash/students', 'middleware' => 'auth'], function() {
-	Route::get('/list', 'StudentController@getList')->name('students.list');
-	Route::get('/new', 'StudentController@getNew')->name('students.new');
-});
