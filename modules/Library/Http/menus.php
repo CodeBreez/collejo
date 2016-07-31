a7f4621ff5ac534c38b99871404050b0cc113d59
Anuradha <astroanu2004@gmail.com>
2016-07-31 16:54:54 +0530

    removed moduled from repo

--- modules/Library/Http/menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Menu::group('library', 'fa-book', function($parent){
-
-	Menu::create('library.list', 'List')->setParent($parent);
-	Menu::create('library.new', 'Create New')->setParent($parent);
-	
-})->setOrder(10);
